b31156a75d367c9b131450ee59cfc5bcaa3411d6
vedanti <vedanti.r.bagade@gmail.com>
2023-10-16 22:50:40 +0530

    file updated

--- src/App.js
+++ src/App.js
@@ -1,59 +1,32 @@
+import { useState } from 'react';
 import './App.css';
-
-const dice = (
-  <div className='face'>
-    <span className='face-1'>
-      <span className='dot'></span>
-    </span>
-    <div className='face-2'>
-      <span className='dot'></span>
-      <span className='dot'></span>
-    </div>
-    <div className='face-3'>
-      <span className='dot'></span>
-      <span className='dot'></span>
-      <span className='dot'></span>
-    </div>
-    <div className='face-4'>
-      <span className='dot'></span>
-      <span className='dot'></span>
-      <span className='dot'></span>
-      <span className='dot'></span>
-    </div>
-    <div className='face-5'>
-      <span className='dot'></span>
-      <span className='dot'></span>
-      <span className='dot'></span>
-      <span className='dot'></span>
-      <span className='dot'></span>
-    </div>
-    <div className='face-6'>
-      <span className='dot'></span>
-      <span className='dot'></span>
-      <span className='dot'></span>
-      <span className='dot'></span>
-      <span className='dot'></span>
-      <span className='dot'></span>
-    </div>
-  </div>
-)
-
-function rollDice() {
-  const newDice = []
-  newDice.push(Math.floor(Math.random()*6))
-  return newDice
-}
+import Dice1 from './Dice_Images/Dice1.png'
+import Dice2 from './Dice_Images/Dice2.png'
+import Dice3 from './Dice_Images/Dice3.png'
+import Dice4 from './Dice_Images/Dice4.png'
+import Dice5 from './Dice_Images/Dice5.png'
+import Dice6 from './Dice_Images/Dice6.png'
 
 function App() {
+
+  function rollDice() {
+    const newDice = Math.floor(Math.random()*6)
+    setNewImage(dice[newDice])
+  }
+
+  const dice = [
+    Dice1, Dice2, Dice3, Dice4, Dice5, Dice6
+  ]
+
+  const [image, setNewImage] = useState(dice[0])
+
   return (
     <div className="App">
       <select className="dropdown">
         <option value="1">•</option>
         <option value="2">••</option>
       </select>
-      <div>
-        {dice}
-      </div>
+      <img src={image} alt="Dice" />
       <div>
           <button onClick={rollDice}>ROLL</button>
       </div>
